009589f6918381ee5492c1d8ab5b1f782d149f83
Pavle Gašić <gasic.pavle14@gmail.com>
2024-11-20 14:50:04 +0100

    initial commit for backend

---
+++ topic-link-backend/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to topic-link-backend
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ topic-link-backend/src/topic_link_backend/core.clj
@@ -0,0 +1,7 @@
+(ns topic-link-backend.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
